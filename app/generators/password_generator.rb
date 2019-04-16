b5f39c2cb4f96513f92be325a3a3332bdeb0c9da
Ben Lee <bendelonlee@gmail.com>
2019-04-16 15:47:24 -0600

    Solves edge case by using the longest word

--- app/generators/password_generator.rb
+++ app/generators/password_generator.rb
@@ -3,11 +3,17 @@ class PasswordGenerator
   File.open('./app/data/common_words.txt').each { |line| COMMON_WORDS.add(line.chomp) }
 
   def make_password(number, fact)
-    fact = fact.delete(number.to_s + '.,').downcase
-    key_words = fact.split().reject do |word|
+    @fact = fact.delete('.,0123456789').downcase
+    @key_words = @fact.split().reject do |word|
       COMMON_WORDS.include?(word)
     end
+    ensure_key_words
+    [@key_words.sample, number].shuffle.join
+  end
 
-    [key_words.sample, number].shuffle.join
+  def ensure_key_words
+    if @key_words.empty?
+      @key_words << @fact.split.max_by{|word| word.length}
+    end
   end
 end
