7b4bc0eb7fb245a8cf6c9571c1c9f44d3add69d2
Ben Lee <bendelonlee@gmail.com>
2019-04-16 14:33:39 -0600

    Removes '.' and upper case letters from fact in password generator

--- app/generators/password_generator.rb
+++ app/generators/password_generator.rb
@@ -3,7 +3,7 @@ class PasswordGenerator
   File.open('./app/data/common_words.txt').each { |line| COMMON_WORDS.add(line.chomp) }
 
   def make_password(number, fact)
-    fact = fact.delete(number.to_s)
+    fact = fact.delete(number.to_s + '.').downcase
     key_words = fact.split().reject do |word|
       COMMON_WORDS.include?(word)
     end
