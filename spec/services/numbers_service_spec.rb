56d19862684b1db53dcdc6a1a9080946ccf1fd33
Ben Lee <bendelonlee@gmail.com>
2019-04-16 11:24:10 -0600

    Service spec passes, returning a number fact dynamically

--- spec/services/numbers_service_spec.rb
+++ spec/services/numbers_service_spec.rb
@@ -1,10 +1,11 @@
-describe NumberService do
+describe NumbersService do
   it "can get a fact" do
-    service = NumberService.new
+    service = NumbersService.new
 
     result = service.get_fact(44)
 
     expect(result).to be_a(String)
+    expect(result).to include('44')
     expect(result.length > 4).to eq(true)
   end
 end
